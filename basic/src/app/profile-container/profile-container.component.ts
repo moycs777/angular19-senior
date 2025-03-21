cbc4694e326070e99a7c168b15d95ef00ce80307
Moises Serrano <moycs777@gmail.com>
2025-03-22 05:23:56 +1100

    feat: template driven forms

--- basic/src/app/profile-container/profile-container.component.ts
+++ basic/src/app/profile-container/profile-container.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-container',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile-container.component.html',
   styleUrl: './profile-container.component.scss'
 })
 export class ProfileContainerComponent {
 
+  nickname = ""
 }
