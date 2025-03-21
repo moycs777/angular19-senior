3b1e72ca500ec4d98b624aaed10f853c87396c9a
Moises Serrano <moycs777@gmail.com>
2025-03-21 11:22:36 -0300

    feat: adding component in the same file

--- basic/src/app/app.component.ts
+++ basic/src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector:'app-user',
+  template:'<h1>I am user</h1>',
+
+})
+export class UserComponent {}
+
+@Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
