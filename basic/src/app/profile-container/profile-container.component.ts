6139a5f35ab7106c5a221055a4676791253eaa92
Moises Serrano <moycs777@gmail.com>
2025-03-22 05:38:45 +1100

    feat: basic reactive forms

--- basic/src/app/profile-container/profile-container.component.ts
+++ basic/src/app/profile-container/profile-container.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-container',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './profile-container.component.html',
   styleUrl: './profile-container.component.scss'
 })
 export class ProfileContainerComponent {
 
   nickname = ""
+
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
+
+  onSubmit(): void {
+    console.log(this.profileForm.value)
+    console.log(this.profileForm.controls.name.value)
+    console.log(this.profileForm.controls.email.value)
+  }
 }
